3c3c8aa7c3f0c3e896e299bfa59315a7cde21b60
Wallace <wlcs@ic.ufal.br>
2024-01-28 18:54:57 -0300

    requirements atualizado e o model denuncia agora aparece na tela admin

--- denuncia/admin.py
+++ denuncia/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from denuncia.models import Denuncia
 
 # Register your models here.
+
+@admin.register(Denuncia)
+class DenunciaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'relato', 'lugar', 'v_fisica', 'v_verbal', 'bullying', 'assedio', 'recorrencia', 'data_ocorrido']
+#Alternativa que faz aparecer tudo podendo mudar os valores: admin.site.register(Denuncia)
