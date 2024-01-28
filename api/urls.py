3b09ef842ad4abf88a402801d71c63d2a32f1739
Wallace <wlcs@ic.ufal.br>
2024-01-28 20:13:57 -0300

    class-based view de detalhar uma denuncia implementada

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('denuncia/', views.DenunciaCreateListView.as_view()),
+    path('denuncia/', views.DenunciaCreateListView.as_view(), name='denuncia-create-list-view'),
+    path('denuncia/<int:pk>/', views.DenunciaRetrieveView.as_view(), name='denuncia-detail-view'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics 
 from denuncia.models import Denuncia
 from .serializers import DenunciaSerializer
-from rest_framework import generics 
 
-
+#Cria uma denuncia e pega a lista de denuncias
 class DenunciaCreateListView(generics.ListCreateAPIView):
+    queryset = Denuncia.objects.all()
+    serializer_class = DenunciaSerializer
+
+#Pega o id da denuncia e retorna os dados da denuncia
+class DenunciaRetrieveView(generics.RetrieveAPIView):
     queryset = Denuncia.objects.all()
     serializer_class = DenunciaSerializer
